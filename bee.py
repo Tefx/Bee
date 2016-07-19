af454b201b87f782f819ac63f4561985ccb7e0de
Zhaomeng Zhu (Tefx) <zhaomeng.zhu@gmail.com>
2016-07-20 03:03:56 +0800

    async

--- bee.py
+++ bee.py
@@ -2,8 +2,9 @@ from __future__ import print_function
 
 import inspect
 
+import gevent
 import msgpack as serializer
-import zmq
+import zmq.green as zmq
 
 
 class RPCMethod(object):
@@ -12,16 +13,24 @@ class RPCMethod(object):
 
 
 class Bee(object):
+    names = []
+    methods = []
+    broker = None
+
+    def process(self, address, msg):
+        (index, argv, kwargs) = serializer.loads(msg)
+        res = self.methods[index](self, *argv, **kwargs) if index >= 0 else self.names
+        self.broker.send_multipart([address, b'', serializer.dumps(res)])
+
     def run(self, address):
-        names, methods = zip(*inspect.getmembers(self, lambda obj: isinstance(obj, RPCMethod)))
-        methods = [m.f for m in methods]
-        content = zmq.Context()
-        sock = content.socket(zmq.REP)
-        sock.bind(address)
+        self.names, methods = zip(*inspect.getmembers(self, lambda obj: isinstance(obj, RPCMethod)))
+        self.methods = [m.f for m in methods]
+        self.broker = zmq.Context().socket(zmq.ROUTER)
+        self.broker.bind(address)
+
         while True:
-            (index, argv, kwargs) = serializer.loads(sock.recv())
-            res = methods[index](self, *argv, **kwargs) if index >= 0 else names
-            sock.send(serializer.dumps(res))
+            address, _, msg = self.broker.recv_multipart()
+            gevent.spawn(self.process, address, msg)
 
 
 class BeeClient(object):
@@ -38,4 +47,31 @@ class BeeClient(object):
         def wrapped(*argv, **kwargs):
             self.sock.send(serializer.dumps((index, argv, kwargs)))
             return serializer.loads(self.sock.recv())
+        return wrapped
+
+
+class AsyncBeeClient(object):
+    def __init__(self, address):
+        self.content = zmq.Context()
+        self.address = address
+        self.query_methods()
+
+    def query_methods(self):
+        sock = self.content.socket(zmq.REQ)
+        sock.connect(self.address)
+        sock.send(serializer.dumps((-1, (), {})))
+        methods = serializer.loads(sock.recv())
+        for index, value in enumerate(methods):
+            setattr(self, value.decode("utf-8"), self.wrap_function(index))
+        sock.close()
+
+    def wrap_function(self, index):
+        def wrapped(*argv, **kwargs):
+            sock = self.content.socket(zmq.REQ)
+            sock.connect(self.address)
+            sock.send(serializer.dumps((index, argv, kwargs)))
+            res = serializer.loads(sock.recv())
+            sock.close()
+            return res
+
         return wrapped
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-from bee import Bee, RPCMethod, BeeClient
+from gevent import sleep
+
+from bee import Bee, RPCMethod, AsyncBeeClient
 
 
 class PointStorage(Bee):
@@ -16,18 +18,34 @@ class PointStorage(Bee):
         return True
 
 
+class Echo(Bee):
+    @RPCMethod
+    def echo(self, x):
+        sleep(1)
+        return x
+
+
 if __name__ == '__main__':
     from sys import argv
 
     addr = "tcp://127.0.0.1:8888"
     # addr = "ipc://tmp.ipc"
 
+    # if argv[1] == "server":
+    #     PointStorage().run(addr)
+    # else:
+    #     c = BeeClient(addr)
+    #     data = [[8.58, 2.29] * 5 for _ in range(100000)]
+    #     if argv[1] == "add":
+    #         print(all(map(c.add, data)))
+    #     elif argv[1] == "add_bulk":
+    #         print(c.add_bulk(data))
+
+    from gevent.pool import Pool
     if argv[1] == "server":
-        PointStorage().run(addr)
+        Echo().run(addr)
     else:
-        c = BeeClient(addr)
-        data = [[8.58, 2.29] * 5 for _ in range(10000)]
-        if argv[1] == "add":
-            print(all(map(c.add, data)))
-        elif argv[1] == "add_bulk":
-            print(c.add_bulk(data))
+        p = Pool()
+        c = AsyncBeeClient(addr)
+        res = p.map(c.echo, range(100))
+        print(list(res))
